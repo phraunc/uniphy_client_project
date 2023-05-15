be6af7d6d566a8ee319027cd1772ddaec113a2a2
Anthony Cole <anthony.cole83@yahoo.com>
2023-05-15 14:57:33 -0500

    Finished with the food form adding the different categories and did some material UI. . Looks good.

--- src/components/Food/FoodForm.jsx
+++ src/components/Food/FoodForm.jsx
@@ -15,6 +15,7 @@ import {
   InputLabel,
   MenuItem,
   Select,
+  TextField,
 } from "@mui/material";
 
 function FoodForm() {
@@ -23,6 +24,9 @@ function FoodForm() {
 
   const [addQuality, setAddQuality] = useState(1);
   const [addQuantity, setAddQuantity] = useState(0);
+  const [addSnack, setAddSnack] = useState(0);
+  const [addWater, setAddWater] = useState(0);
+  const [addFasting, setAddFasting] = useState(0);
 
   const handleHome = () => {
     // console.log("history test");
@@ -32,17 +36,15 @@ function FoodForm() {
   const addFood = () => {
     console.log("adFood");
   };
-  const handleChange = (event) => {
-    setAddQuantity(event.target.value);
-    console.log("handeChange");
-  };
-
+ 
   return (
     <>
       <h1>Food Form</h1>
       <div>
         <form onSubmit={addFood}>
-          <input
+          <TextField
+          label="Food Quality"
+          variant="outlined"
             type="number"
             placeholder="1-100"
             min="1"
@@ -50,6 +52,8 @@ function FoodForm() {
             value={addQuality}
             onChange={(event) => setAddQuality(event.target.value)}
           />
+          <br/>
+          <br/>
           <Box sx={{ minWidth: 120 }}>
             <FormControl fullWidth>
               <InputLabel id="demo-simple-select-label">Quantity</InputLabel>
@@ -58,7 +62,7 @@ function FoodForm() {
                 id="demo-simple-select"
                 value={addQuantity}
                 label="quantity"
-                onChange={handleChange}
+                onChange={(event)=> setAddQuantity(event.target.value)}
               >
                 <MenuItem value={-5}>Very Hungry</MenuItem>
                 <MenuItem value={-4}>Moderately Hungry</MenuItem>
@@ -73,8 +77,75 @@ function FoodForm() {
                 <MenuItem value={5}>Very Full...AMERICA!!</MenuItem>
               </Select>
             </FormControl>
+            <br />
+            <br />
+            <FormControl fullWidth>
+              <InputLabel id="demo-simple-select-label">Snacks</InputLabel>
+              <Select
+                labelId="demo-simple-select-label"
+                id="demo-simple-select"
+                value={addSnack}
+                label="snacks"
+                onChange={(event)=>setAddSnack(event.target.value)}
+              >
+                <MenuItem value={0}>No Snacks</MenuItem>
+                <MenuItem value={1}>One Snack</MenuItem>
+                <MenuItem value={2}>Two Snacks</MenuItem>
+                <MenuItem value={3}>Three Snacks</MenuItem>
+                <MenuItem value={4}>Four Snacks</MenuItem>
+                <MenuItem value={5}>Five or More Snacks</MenuItem>
+              </Select>
+            </FormControl>
+            <br />
+            <br />
+            <FormControl fullWidth>
+              <InputLabel id="demo-simple-select-label">Water</InputLabel>
+              <Select
+                labelId="demo-simple-select-label"
+                id="demo-simple-select"
+                value={addWater}
+                label="water"
+                onChange={(event)=>setAddWater(event.target.value)}
+              >
+                <MenuItem value={0}>0 Ltr</MenuItem>
+                <MenuItem value={1}>.5 Ltr</MenuItem>
+                <MenuItem value={2}>1 Ltr</MenuItem>
+                <MenuItem value={3}>1.5 Ltr</MenuItem>
+                <MenuItem value={4}>2 Ltr</MenuItem>
+                <MenuItem value={5}>2.5 Ltr</MenuItem>
+              </Select>
+            </FormControl>
+            <br />
+            <br />
+            <FormControl fullWidth>
+              <InputLabel id="demo-simple-select-label">Fasting</InputLabel>
+              <Select
+                labelId="demo-simple-select-label"
+                id="demo-simple-select"
+                value={addFasting}
+                label="fasting"
+                onChange={(event)=>setAddFasting(event.target.value)}
+              >
+                <MenuItem value={0}>Fewer Then 12</MenuItem>
+                <MenuItem value={1}>12-14 Hrs</MenuItem>
+                <MenuItem value={2}>14-16 Hrs</MenuItem>
+                <MenuItem value={3}>16-24 Hrs</MenuItem>
+                <MenuItem value={4}>24+ Hrs</MenuItem>
+              </Select>
+            </FormControl>
+            <br/>
+            <br/>
+            <Box
+                m={1}
+                mt={3}
+                display="flex"
+                justifyContent="flex-end"
+                alignItems="flex-end">
+            <Button variant="contained"  type="submit" >Submit</Button>
+            </Box>
           </Box>
-     
+      
+
         </form>
       </div>
     </>
