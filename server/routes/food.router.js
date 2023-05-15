9c90af2e756187168202f124e0c45c6428ea7d5d
Anthony Cole <anthony.cole83@yahoo.com>
2023-05-15 11:53:35 -0500

    made our delete route for food.

--- server/routes/food.router.js
+++ server/routes/food.router.js
@@ -1,5 +1,8 @@
 const express = require("express");
 const pool = require("../modules/pool");
+const {
+  rejectUnauthenticated,
+} = require("../modules/authentication-middleware");
 const router = express.Router();
 
 /**
@@ -22,19 +25,42 @@ router.get("/", (req, res) => {
 /**
  * POST route template
  */
-router.post("/", (req, res) => {
+router.post("/", rejectUnauthenticated, (req, res) => {
   // POST route code here
-  const sqlText= `INSERT INTO food (user_id, quality, quantity, snack, water, fasting)
-  VALUES ($1, $2, $3, $4, $5, $6);`
-  const sqlValue = [req.user.id, req.body.quality, req.body.quantity,req.body.snack, req.body.water, req.body.fasting];
-  pool.query(sqlText, sqlValue)
-  .then((result)=>{
-    res.sendStatus(200)
-  }).catch((err)=>{
-    console.log('error in post rout food', err)
-    res.sendStatus(500)
-  })
-  
+  const sqlText = `INSERT INTO food (user_id, quality, quantity, snack, water, fasting)
+  VALUES ($1, $2, $3, $4, $5, $6);`;
+  const sqlValue = [
+    req.user.id,
+    req.body.quality,
+    req.body.quantity,
+    req.body.snack,
+    req.body.water,
+    req.body.fasting,
+  ];
+  pool
+    .query(sqlText, sqlValue)
+    .then((result) => {
+      res.sendStatus(200);
+    })
+    .catch((err) => {
+      console.log("error in post rout food", err);
+      res.sendStatus(500);
+    });
+});
+
+router.delete("/:id", rejectUnauthenticated, (req, res) => {
+  const sqlText = `DELETE FROM "food" WHERE "food".id = $1;`;
+  const sqlValue = [req.params.id];
+
+  pool
+    .query(sqlText, sqlValue)
+    .then(() => {
+      res.sendStatus(200);
+    })
+    .catch((err) => {
+      console.log("err deleting router food", err);
+      res.sendStatus(500);
+    });
 });
 
 module.exports = router;
