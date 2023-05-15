b672a1f7ed3daf39705a98016aa1b8584820e393
Anthony Cole <anthony.cole83@yahoo.com>
2023-05-15 13:30:12 -0500

    made a food component and added the link to the nav component as well as the app.jsx.  We also included a h1 element with 3 divs for food, history and add food.

---
+++ src/components/Food/Food.jsx
@@ -0,0 +1,30 @@
+import { useEffect, useState } from "react";
+import { useSelector, useDispatch } from "react-redux";
+import { useHistory } from "react-router-dom";
+
+function Food() {
+  const history = useHistory();
+  const dispatch = useDispatch();
+
+  const handleHome = () => {
+    console.log("history test");
+    history.push("/home");
+  };
+
+  return (
+    <>
+      <h1>Food</h1>
+      <div>
+        <button onClick={handleHome}>back</button>
+      </div>
+      <div>
+        <p>History</p>
+      </div>
+      <div>
+        <button>Add Food</button>
+      </div>
+    </>
+  );
+}
+
+export default Food;
--- src/components/Nav/Nav.jsx
+++ src/components/Nav/Nav.jsx
@@ -1,8 +1,8 @@
-import React from 'react';
-import { Link } from 'react-router-dom';
-import LogOutButton from '../LogOutButton/LogOutButton';
-import './Nav.css';
-import { useSelector } from 'react-redux';
+import React from "react";
+import { Link } from "react-router-dom";
+import LogOutButton from "../LogOutButton/LogOutButton";
+import "./Nav.css";
+import { useSelector } from "react-redux";
 
 function Nav() {
   const user = useSelector((store) => store.user);
@@ -30,6 +30,10 @@ function Nav() {
 
             <Link className="navLink" to="/info">
               Info Page
+            </Link>
+            
+            <Link className="navLink" to="/food">
+              Food
             </Link>
 
             <LogOutButton className="navLink" />
