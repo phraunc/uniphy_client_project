6c459668c9b08835c7a9c35756f4d5f804653362
Steven Gangl <steven.h.gangl@gmail.com>
2023-05-22 10:39:49 -0500

    added work pillar to landing page and connected the route and logic fot it

--- src/components/Work/WorkForm.jsx
+++ src/components/Work/WorkForm.jsx
@@ -24,9 +24,9 @@ function WorkForm() {
     const dispatch = useDispatch();
 
     const [addNote, setAddNote] = useState(0)
-    const [addWorkload, setAddWorkload] = useState(0)
-    const [addFullfillment, setAddFullfillment] = useState(0)
- 
+    const [addWorkload, setAddWorkload] = useState()
+    const [addFullfillment, setAddFullfillment] = useState()
+
 
 
     const handleHome = () => {
@@ -34,20 +34,31 @@ function WorkForm() {
         history.push("/home");
     };
 
-    const addWork = (event) => {
+    async function addWork(event) {
         event.preventDefault();
-   console.log('addnote', addNote, 'addWorkload', addWorkload, 'addFullfillment', addFullfillment);
+        const calculatedWorkScore = await workScoreCalc()
+        console.log('addnote', addNote, 'addWorkload', addWorkload, 'addFullfillment', addFullfillment);
+
         dispatch({
             type: 'POST_WORK',
             payload: {
                 note: addNote,
                 workload: addWorkload,
                 fullfillment: addFullfillment,
+                score_w: calculatedWorkScore.wScore,
+                total_points: calculatedWorkScore.totalBalancePoints
+
             }
         })
         setAddNote(0)
         setAddWorkload(0)
         setAddFullfillment(0)
+        dispatch({
+            type: "UPDATE_WORK_SCORE",
+            payload: {
+              score_w:calculatedWorkScore.wScore,
+            }
+          })
 
         history.push("/work");
     };
@@ -56,44 +67,161 @@ function WorkForm() {
         history.push("/work")
     }
 
+    async function workScoreCalc() {
+        let fulfillmentPoints = 0
+        let workLoadPoints = 0
+        let totalBalancePoints = 0
+
+        switch (addWorkload) {
+            case -5:
+                workLoadPoints = 0
+                break;
+            case -4:
+                workLoadPoints = 10
+                break;
+            case -3:
+                workLoadPoints = 20
+                break;
+            case -2:
+                workLoadPoints = 30
+                break;
+            case -1:
+                workLoadPoints = 40
+                break;
+
+            case 0:
+                workLoadPoints = 50
+                break;
+            case 1:
+                workLoadPoints = 60
+                break;
+            case 2:
+                workLoadPoints = 70
+                break;
+            case 3:
+                workLoadPoints = 80
+                break;
+            case 4:
+                workLoadPoints = 90
+                break;
+            case 5:
+                workLoadPoints = 100
+                break;
+            default:
+                workLoadPoints = 0
+        }
+        switch (addFullfillment) {
+            case 0:
+                fulfillmentPoints = 1
+                break;
+            case 1:
+                fulfillmentPoints = 1.1
+                break;
+            case 2:
+                fulfillmentPoints = 1.2
+                break;
+            case 3:
+                fulfillmentPoints = 1.3
+                break;
+            case 4:
+                fulfillmentPoints = 1.4
+                break;
+            case 5:
+                fulfillmentPoints = 1.5
+                break;
+            case 6:
+                fulfillmentPoints = 1.6
+                break;
+            case 7:
+                fulfillmentPoints = 1.7
+                break;
+            case 8:
+                fulfillmentPoints = 1.8
+                break;
+            case 9:
+                fulfillmentPoints = 1.9
+                break;
+            case 10:
+                fulfillmentPoints = 2
+                break;
+
+            default:
+                fulfillmentPoints = 1
+        }
+
+        totalBalancePoints = Number((workLoadPoints * fulfillmentPoints).toFixed(2))
+        let wScore = 0
+        if (totalBalancePoints > 100) {
+            wScore = 100
+        } else if (totalBalancePoints < 0) {
+            wScore = 0
+        } else {
+            wScore = totalBalancePoints
+        }
+        return (
+            {
+                wScore,
+                totalBalancePoints
+            }
+
+        )
+    }
+
+
+
+
     return (
         <>
 
-            <h1>Work Form</h1>
             <div>
                 <form onSubmit={addWork}>
+
+                    <FormControl fullWidth>
+                        <InputLabel id="demo-simple-select-label">Workload</InputLabel>
+                        <Select
+                            labelId="demo-simple-select-label"
+                            id="demo-simple-select"
+                            value={addWorkload}
+                            label="quantity"
+                            onChange={(event) => setAddWorkload(event.target.value)}
+                        >
+                            <MenuItem value={-5}>Feeling Buried</MenuItem>
+                            <MenuItem value={-4}>Drowning In Tasks</MenuItem>
+                            <MenuItem value={-3}>Burning The Candle At Both Ends</MenuItem>
+                            <MenuItem value={-2}>Running On Empty</MenuItem>
+                            <MenuItem value={-1}>Treading Water</MenuItem>
+                            <MenuItem value={0}>Staying Afloat</MenuItem>
+                            <MenuItem value={1}>Finding Balance</MenuItem>
+                            <MenuItem value={2}>In The Groove</MenuItem>
+                            <MenuItem value={3}>Smooth Sailing</MenuItem>
+                            <MenuItem value={4}>Master Of The Craft</MenuItem>
+                            <MenuItem value={5}>Overdrive</MenuItem>
+
+                        </Select>
+                    </FormControl>
+                    <br />
+                    <br />
                     <TextField
-                        label="Work Note"
+                        label="Work Fullfillment"
                         variant="outlined"
-                        type="text"
-                        placeholder="Notes"
-                        value={addNote}
-                        onChange={(event) => setAddNote(event.target.value)}
+                        type="number"
+                        placeholder="1 - 10"
+                        min="1"
+                        max="10"
+                        value={addFullfillment}
+                        onChange={(event) => setAddFullfillment(Number(event.target.value))}
                     />
                     <br />
                     <br />
                     <Box sx={{ minWidth: 120 }}>
                         <TextField
-                            label="Workload"
+                            label="Notes"
                             variant="outlined"
-                            type="number"
-                            placeholder="Workload?"
-                            min="1"
-                            max="10"
-                            value={addWorkload}
-                            onChange={(event) => setAddWorkload(event.target.value)}
-                        />
-                        <br />
-                        <br />
-                        <TextField
-                            label="Work Fullfillment"
-                            variant="outlined"
-                            type="number"
-                            placeholder="Fullfilled?"
-                            min="1"
-                            max="10"
-                            value={addFullfillment}
-                            onChange={(event) => setAddFullfillment(event.target.value)}
+                            type="text"
+                            placeholder="Notes"
+
+                            value={addNote}
+                            onChange={(event) => setAddNote(event.target.value)}
                         />
                         <br />
                         <br />
