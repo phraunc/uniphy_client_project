d5d4e4d6a8c2f7bd84207f13c4431a37a307a567
Anthony Cole <anthony.cole83@yahoo.com>
2023-05-22 11:24:54 -0500

    updated from my last commit returning only three cards

--- src/components/HistoryContainer/SleepHistoryContainer.jsx
+++ src/components/HistoryContainer/SleepHistoryContainer.jsx
@@ -5,6 +5,7 @@ function SleepHistoryContainer({ item }) {
   return (
     <div className="flex-grid-quarters">
       {item.map((single, i) => {
+         if (i > 2) return null;
         return (
           <div className="container" key={i} >
             <HistoryCardSleep  prop={single} />
