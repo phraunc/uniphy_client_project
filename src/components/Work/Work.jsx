6b65a1b01df0c3d070b2af9a77b5f19d29e03fba
Steven Gangl <steven.h.gangl@gmail.com>
2023-05-23 09:43:30 -0500

    added circle slider to work pillar

--- src/components/Work/Work.jsx
+++ src/components/Work/Work.jsx
@@ -1,21 +1,33 @@
-import { useEffect, useState } from "react";
+import { useEffect, useState, useRef } from "react";
 import { useSelector, useDispatch } from "react-redux";
 import { useHistory } from "react-router-dom";
 import HistoryWorkContainer from "../HistoryContainer/HistoryWorkContainer"
 import backIcon from '../img/backIcon.png'
 import manualAddWorkBtn from '../img/manualAddWorkBtn.png';
+import { CircleSlider } from "react-circle-slider";
+
+
+
 function Work() {
     const history = useHistory();
     const dispatch = useDispatch();
 
     const workStore = useSelector(store => store.rootWorkReducer.workReducer)
+    const BS = useSelector((store) => store.balanceScoreReducer);
+
     const [keyValue, setKeyValue] = useState(0)
+    const slider = useRef(null);
+
+
 
     useEffect(() => {
         dispatch({
             type: "GET_WORK"
         })
-        
+        dispatch({
+            type: "GET_BALANCE_SCORE"
+        })
+
     }, [])
 
     const handleHome = () => {
@@ -33,6 +45,30 @@ function Work() {
                 <img src={backIcon} alt="backButton" onClick={handleHome} />
             </div>
             <div key={keyValue} style={{ backgroundColor: "white" }}>
+
+                <center>
+                    <div className="App1">
+                        <div className="textContainer">
+                            {Math.round(Number(BS.score_w))}
+                            <div className="minute">Work Score</div>
+                        </div>
+                        <CircleSlider
+                            ref={slider}
+                            value={Math.round(Number(BS.score_w))}
+                            stepSize={5}
+                            // onChange={value => changeValue(value)}
+                            size={250}
+                            max={100}
+                            gradientColorFrom="#ec008c"
+                            gradientColorTo="#31356e"
+                            knobRadius={20}
+                            circleWidth={20}
+                            disabled={true}
+                        />
+                    </div>
+                </center>
+
+
                 <div>
                     <HistoryWorkContainer item={workStore} />
                 </div>
